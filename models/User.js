66f3c2c677269bf202dcd920f76c0b60b0f18be8
nashera28bomu-pixel <nashera28bomu@gmail.com>
2026-07-25 16:16:52 +0300

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -4,6 +4,8 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
+  lastLessonNumber: { type: Number, default: null },
+  lastAccessedAt: { type: Date, default: null },
   createdAt: { type: Date, default: Date.now }
 });
 
